8092f588fb797539fe14cc7e31cbf811f091f042
baoluan <baoluan@thuvienphapluat.net>
2022-12-24 14:51:54 +0700

    update

--- src/store/store.js
+++ src/store/store.js
@@ -17,13 +17,17 @@ const loggerMiddleware = (store) => (next) => (action) => {
   
     console.log('next state: ', store.getState());
 };
-
-const middleWares = [logger];
+;
 // const middleWares = [loggerMiddleware];
+const middleWares = [process.env.NODE_ENV === 'development' && logger].filter(
+    Boolean
+);
 
-// const middleWares = [process.env.NODE_ENV === 'development' && logger].filter(
-//     Boolean
-//   );
+const composeEnhancer =
+  (process.env.NODE_ENV !== 'production' &&
+    window &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+  compose;
 
 const persistConfig = {
     key: "root",
@@ -33,7 +37,7 @@ const persistConfig = {
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-const composedEnhancers = compose(applyMiddleware(...middleWares));
+const composedEnhancers = composeEnhancer(applyMiddleware(...middleWares));
 
 // export const store = createStore(rootReducer, undefined, composedEnhancers);
 export const store = createStore(persistedReducer, undefined, composedEnhancers);
